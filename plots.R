b1e257a6ce39a3505bac0e300e5d5a971f4f3555
Ankit Mukherjee <ankit0204.am@gmail.com>
2026-05-23 12:30:48 +0530

    updated_plots

--- plots.R
+++ plots.R
@@ -4,6 +4,7 @@ library(readr)
 library(readxl)
 library(ComplexHeatmap)
 library(circlize)
+library(gridtext)
 
 # Figure 4d
 # Similar for plots S13.1, S13.2, S13.3, S13.4
@@ -14,8 +15,8 @@ imp_var <- imp_var |>
   mutate(pop_num = suppressWarnings(as.numeric(populations))) |>
   arrange(is.na(pop_num), pop_num) |>
   mutate(populations = factor(populations, levels = c(as.character(1:82), "SAS", "AFR", "AMR", "EAS", "EUR")),
-         type = factor(type, levels = c("Toxicity", "Dosage", "Efficacy", "Other")),
-         variant_gene = paste0(variant, " (", gene, ")"),
+         type = factor(type, levels = c("Dosage", "Toxicity", "Efficacy", "Other")),
+         variant_gene = paste0(variant, " (<i>", gene, "</i>)"),
          chemicals = str_to_title(chemicals),
          drug_category = paste0(chemicals, " (", category, ")"),
          Linguistic_group_Tribe = factor(Linguistic_group_Tribe,
@@ -43,16 +44,7 @@ linguistic_colors <- c(
   "IE_T" = "#cd3333", "IE_NT" = "#FAC4C4", "TB_T" = "#7ccd7c",
   "NT" = "#C7F2C7", "Global" = "#000000"
 )
-type_colors <- c("Toxicity"="#FF9999", "Dosage"="#99CCFF", "Efficacy"="#99FF99", "Other"="#CCCCCC")
-bio_geo_colors <- c("Northern Riverine Plains" = "#289D3D", "Western Plains" = "#735323", 
-                    "Western Himalayas" = "#C0F1FF", "Western Coastal Plains" = "#9AF708", 
-                    "Eastern Riverine Plains" = "#00FFC1", "Eastern Coastal Plains" = "#43582F", 
-                    "North Deccan" = "#F8C1A6", "North Central Highlands" = "#EDF181", 
-                    "South Central Highlands" = "#A5506D", "South Deccan" = "#BCCB34", 
-                    "Eastern Ghats" = "#D1AAC2", "Western Ghats" = "#DB7003", 
-                    "Nilgiri Hills" = "#7B9EFF", "Eastern Plateau" = "#FBA600", 
-                    "Brahmaputra Valley" = "#328C97", "Central Himalayas" = "#FCFF00", 
-                    "North Eastern Range" = "#BBA980", "Global" = "#000000")
+type_colors <- c("Toxicity"="#c6caca", "Dosage"="#c6caca", "Efficacy"="#c6caca", "Other"="#c6caca")
 
 min_val <- min(imp_var$norm_frequency, na.rm = TRUE)
 max_val <- max(imp_var$norm_frequency, na.rm = TRUE)
@@ -62,20 +54,18 @@ col_fun <- colorRamp2(
   c("#2E627AFF", "#1283c0ff", "#F5F5F5FF", "#D6604DFF", "#B2182BFF", "#67001FFF")
 )
 
-bio_geo_data <- imp_var |> distinct(populations, Biogeography)
-bio_geo_data <- bio_geo_data[match(colnames(mat), bio_geo_data$populations), ]
-
-row_meta <- imp_var |>
+row_meta <- imp_var %>%
   distinct(variant_gene, drug_category, type, Testing)
 row_meta <- row_meta[match(rownames(mat), row_meta$variant_gene), ]
 
-col_meta <- imp_var |>
-  distinct(populations, Linguistic_group_Tribe, Biogeography)
+col_meta <- imp_var %>%
+  distinct(populations, Linguistic_group_Tribe)
 col_meta <- col_meta[match(colnames(mat), col_meta$populations), ]
 
 col_ha <- HeatmapAnnotation(
   Linguistic_Group = col_meta$Linguistic_group_Tribe,
   col = list(Linguistic_Group = linguistic_colors),
+  spacer = anno_empty(height = unit(1, "mm"), border = FALSE),
   annotation_name_gp = gpar(col = NA),
   show_legend = FALSE
 )
@@ -89,7 +79,7 @@ right_ha <- rowAnnotation(
 
 left_ha <- rowAnnotation(
   Type = row_meta$type,
-  Variant = anno_text(rownames(mat), 
+  Variant = anno_text(gt_render(rownames(mat)), 
                       gp = gpar(fontsize = 32, fontface = "bold",
                                 col = testing_colors[row_meta$Testing]),
                       just = "right",
@@ -109,15 +99,12 @@ ht <- Heatmap(
   show_row_names = FALSE,
   show_column_names = TRUE,
   row_split = row_meta$type,
-  row_title_gp = gpar(fontsize = 22, fontface = "bold"),
+  row_title_gp = gpar(fontsize = 30, fontface = "bold"),
   column_split = col_meta$Linguistic_group_Tribe,
   column_title_gp = gpar(fontsize = 28, fontface = "bold"),
-  column_names_gp = gpar(
-    fontsize = 30, fontface = "bold",
-    col = bio_geo_colors[col_meta$Biogeography]
-  ),
-  row_gap = unit(2, "mm"),
-  column_gap = unit(2, "mm"),
+  column_names_gp = gpar(fontsize = 30, fontface = "bold"),
+  row_gap = unit(3, "mm"),
+  column_gap = unit(3, "mm"),
   top_annotation = col_ha,
   right_annotation = right_ha,
   left_annotation = left_ha,
@@ -145,7 +132,7 @@ lg_testing <- Legend(
   legend_gp = gpar(fill = testing_colors),
   direction = "horizontal",
   title_gp = gpar(fontface = "bold", fontsize = 28),
-  labels_gp = gpar(fontsize = 22),
+  labels_gp = gpar(fontsize = 26),
   title_gap = unit(5, "mm"),
   row_gap = unit(2, "mm")
 )
@@ -156,8 +143,7 @@ combined_legends <- packLegend(
   gap = unit(6, "cm")
 )
 
-png("final_heatmap.png",
-    width = 60, height = 15, units = "in", res = 600)
+png("final_heatmap.png", width = 60, height = 15, units = "in", res = 600)
 
 draw(
   ht,
@@ -170,34 +156,40 @@ draw(
 dev.off()
 
 # Figure 4e, 4f
-cyp_genes <- c("cyp2b6", "cyp2c9", "cyp3a5", "cyp2c19", "cyp4f2")
+cyp_genes <- c("cyp2b6", "cyp2c9", "cyp3a5", "cyp2c19", "cyp4f2", "cyp2d6")
 non_cyp_genes <- c("cftr", "slco1b1", "dpyd", "tpmt", "ifnl3", "ugt1a1", "nudt15", "vkorc1")
 
-pop_info <- read_excel("pop_info.xlsx") |>
-  select(FID, Linguistic_group_Tribe) |> 
+pop_info <- read_excel("final_pop_info.xlsx") %>%
+  select(FID, Linguistic_group_Tribe) %>% 
   distinct()
 
-cyp2d6 <- read_excel("merged_genotypes.xlsx") |>
-  rename(name = FID, phenotype = Metabolizer_Status) |> 
-  select(name, Linguistic_group_Tribe, phenotype) |> 
+cyp2d6 <- read_excel("cyp2d6_merged_genotypes.xlsx") %>%
+  rename(name = FID, phenotype = Metabolizer_Status) %>% 
+  select(name, Linguistic_group_Tribe, phenotype) %>% 
   mutate(gene = "cyp2d6")
+
+cyrius_samples <- cyp2d6$name
 
 merged_rows_df <- data.frame()
 
 for (gene in c(cyp_genes, non_cyp_genes)) {
-  star_allele <- file.path("stargazer\\",
-                           paste0("output-", gene, ".sg-genotype.txt"))
+  star_allele <- file.path("stargazer/", paste0("output-", gene, ".sg-genotype.txt"))
   
   if (!file.exists(star_allele)) {
     message("Gene not genotyped: ", gene)
     next
   }
   
-  df <- read_tsv(star_allele, col_types = cols(.default = "c")) |>
-    select(name, phenotype) |> 
+  df <- read_tsv(star_allele, col_types = cols(.default = "c")) %>%
+    select(name, phenotype) %>% 
     mutate(gene = gene)
   
-  merged_df <- merge(df, pop_info, by.x = "name", by.y = "FID") |>
+  if(gene == "cyp2d6") {
+  df <- df %>%
+    filter(!name %in% cyrius_samples)
+  }
+  
+  merged_df <- merge(df, pop_info, by.x = "name", by.y = "FID") %>%
     filter(Linguistic_group_Tribe != "")
   
   merged_rows_df <- bind_rows(merged_rows_df, merged_df)
@@ -205,60 +197,141 @@ for (gene in c(cyp_genes, non_cyp_genes)) {
 
 final_df <- bind_rows(merged_rows_df, cyp2d6)
 
-pop_gene_phenotype <- final_df |>
-  group_by(Linguistic_group_Tribe, gene, phenotype) |>
-  summarise(count = n(), .groups = "drop") |>
-  group_by(Linguistic_group_Tribe, gene) |>
+pop_gene_phenotype <- final_df %>%
+  group_by(Linguistic_group_Tribe, gene, phenotype) %>%
+  summarise(count = n(), .groups = "drop") %>%
+  group_by(Linguistic_group_Tribe, gene) %>%
   mutate(freq = (count / sum(count)) * 100)
 
-pop_gene_phenotype <- pop_gene_phenotype |>
+pop_gene_phenotype <- pop_gene_phenotype %>%
   mutate(phenotype = recode(phenotype,
-                            "normal_metabolizer" = "Normal Metabolizer",
-                            "normal_function" = "Normal Metabolizer",
-                            "favorable_response" = "Normal Metabolizer",
-                            "intermediate_metabolizer" = "Intermediate Metabolizer",
-                            "poor_metabolizer" = "Poor Metabolizer",
-                            "poor_function" = "Poor Metabolizer",
-                            "decreased_function" = "Poor Metabolizer",
-                            "rapid_metabolizer" = "Rapid Metabolizer",
-                            "increased_function" = "Rapid Metabolizer",
-                            "ultrarapid_metabolizer" = "Ultrarapid Metabolizer",
-                            "unknown_metabolizer" = "Unknown Metabolizer",
-                            "unknown_function" = "Unknown Metabolizer",
-                            "Indeterminate" = "Unknown Metabolizer"),
-         Linguistic_group_Tribe = factor(Linguistic_group_Tribe, levels = c("AA_T", "DR_T", "DR_NT",
-                                                                            "IE_T", "IE_NT", "TB_T",
-                                                                            "NT")),
+                            "normal_metabolizer" = "Normal",
+                            "Normal Metabolizer" = "Normal",
+                            "normal_function" = "Normal",
+                            "favorable_response" = "Normal",
+                            "intermediate_metabolizer" = "Intermediate",
+                            "Intermediate Metabolizer" = "Intermediate",
+                            "poor_metabolizer" = "Poor",
+                            "Poor Metabolizer" = "Poor",
+                            "poor_function" = "Poor",
+                            "decreased_function" = "Poor",
+                            "rapid_metabolizer" = "Rapid",
+                            "increased_function" = "Rapid",
+                            "ultrarapid_metabolizer" = "Rapid",
+                            "Ultrarapid Metabolizer" = "Rapid",
+                            "unknown_metabolizer" = "Unknown",
+                            "Unknown Metabolizer" = "Unknown",
+                            "unknown_function" = "Unknown",
+                            "Indeterminate" = "Unknown"),
+         Linguistic_group_Tribe = recode(Linguistic_group_Tribe, 
+                            "AA_Tribal" = "AA_T",
+                            "DR_Non_Tribal" = "DR_NT",
+                            "DR_Tribal" = "DR_T",
+                            "IE_Non_Tribal" = "IE_NT",
+                            "IE_Tribal" = "IE_T",
+                            "TB_Non_Tribal" = "TB_NT",
+                            "TB_Tribal" = "TB_T",
+                            "CAO" = "CAO"),
+          Linguistic_group_Tribe = factor(Linguistic_group_Tribe, levels = rev(c("AA_T", "DR_NT", "DR_T", "IE_NT",
+                                                                            "IE_T", "TB_NT", "TB_T", "CAO"))),
          gene = toupper(as.character(gene)))
 
-phenotype_colors <- c(
-  "Normal Metabolizer" = "#1f77b4",
-  "Intermediate Metabolizer" = "#ff7f0e",
-  "Poor Metabolizer" = "#d62728",
-  "Rapid Metabolizer" = "#2ca02c",
-  "Ultrarapid Metabolizer" = "#9467bd",
-  "Unknown Metabolizer" = "grey70"
+gene_order <- c(
+  "CYP2B6",
+  "CYP2C9",
+  "CYP3A5",
+  "CYP2C19",
+  "CYP4F2",
+  "CYP2D6",
+  "CFTR",
+  "SLCO1B1",
+  "DPYD",
+  "TPMT",
+  "IFNL3",
+  "UGT1A1",
+  "NUDT15",
+  "VKORC1"
 )
 
-p <- ggplot(pop_gene_phenotype, aes(x = freq, y = gene, fill = phenotype)) +
-  geom_bar(stat = "identity", position = "stack") +
-  facet_grid(gene ~ Linguistic_group_Tribe, scales = "free_y", space = "free_y") +
-  scale_y_discrete(expand = expansion(add = c(0, 0))) +
+pop_gene_phenotype$gene <- factor(
+  pop_gene_phenotype$gene,
+  levels = gene_order
+)
+
+phenotype_colors <- c(
+  "Normal" = "#1f77b4",
+  "Intermediate" = "#ff7f0e",
+  "Poor" = "#d62728",
+  "Rapid" = "#2ca02c",
+  "Unknown" = "grey70"
+)
+
+p <- ggplot(pop_gene_phenotype, aes(x = freq, y = forcats::fct_rev(gene), fill = phenotype)) +
+  geom_bar(stat = "identity", width = 0.8) +
   scale_fill_manual(values = phenotype_colors) +
+  scale_x_continuous(expand = c(0, 0)) +
+  labs(x = "Proportion", y = NULL, fill = "Metabolizer") +
   theme_minimal() +
   theme(
-    axis.title = element_blank(),
+    panel.grid = element_blank(),
     axis.text.x = element_blank(),
-    axis.text.y.left = element_text(vjust = 0.5, face = "bold", size = 10),
-    strip.text.x.top = element_text(face = "bold", size = 10),
-    strip.text.y.right = element_blank(),
+    axis.ticks.x = element_blank(),
+    axis.text.y = element_text(face = "bold.italic", size = 6),
+    axis.title.x = element_text(size = 8),
     legend.position = "right",
-    legend.title = element_blank(),
-    panel.spacing.x = unit(-0.1, "cm")
+    legend.text = element_text(size = 6),
+    legend.key.size = unit(0.3, "cm"),
+    legend.title = element_text(size = 6),
+    panel.spacing.x = unit(0.08, "cm"),
+    panel.spacing.y = unit(-0.02, "cm")
   )
 
-ggsave("pop_gene_matrix.jpeg",
-       p, device = "jpeg", height = 5, width = 12, dpi = 600)
+ggsave(
+  filename = "gene_matrix_main.png",
+  plot = q,
+  device = "png",
+  width = 3,
+  height = 2.8,
+  units = "in",
+  dpi = 600,
+  bg = "white"
+)
+
+pop_gene_phenotype <- pop_gene_phenotype %>% filter(gene %in% c("CYP2B6", "CYP2C19", "CYP2D6", "CYP3A5", "CYP4F2", "SLCO1B1", "UGT1A1", "VKORC1"))
+
+q <- ggplot(pop_gene_phenotype, aes(x = freq, y = Linguistic_group_Tribe, fill = phenotype)) +
+  geom_bar(stat = "identity", width = 0.8) +
+  facet_grid(. ~ gene, scales = "free_x", space = "free_x") +
+  scale_fill_manual(values = phenotype_colors) +
+  scale_x_continuous(expand = c(0, 0)) +
+  labs(x = "Proportion", y = NULL, fill = "Metabolizer") +
+  theme_minimal() +
+  theme(
+    panel.grid = element_blank(),
+    axis.text.x = element_blank(),
+    axis.ticks.x = element_blank(),
+    axis.text.y = element_text(face = "bold", size = 6),
+    axis.title.x = element_text(size = 8),
+    strip.background = element_rect(fill = "grey85", color = "grey50"),
+    strip.text.x = element_text(face = "bold.italic", size = 6),
+    legend.position = "bottom",
+    legend.text = element_text(size = 6),
+    legend.key.size = unit(0.3, "cm"),
+    legend.title = element_text(size = 6),
+    panel.spacing.x = unit(0.08, "cm"),
+    panel.spacing.y = unit(-0.02, "cm")
+  )
+
+ggsave(
+  filename = "pop_gene_matrix_main.png",
+  plot = r,
+  device = "png",
+  width = 9,
+  height = 2.8,
+  units = "in",
+  dpi = 600,
+  bg = "white"
+)
 
 # Figure S13.4
 actionable <- read_excel("actionable_per_sample.xlsx", sheet = "Total_Actionable")
